47fe6fa1916d5bcb3dd34e751b1d6edb915b3b09
FranOlivec <francisca.olivares.vecchiola@gmail.com>
2023-08-27 20:53:29 -0400

    mod. vistas y mod modelos agregación de roles

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   resources :comments
   resources :publications
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions', 
+    registration: 'users/registrations'
+  }
   get 'homes/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "homes#index"
-  get '/publications', to: 'publications#index', as:'user_root'
+  root "publications#index"
+  
 end
